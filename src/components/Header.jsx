e77fd625f02c7623f558adcacb6b596c2056da7d
sergiotto <sergiotto@outlook.com>
2022-12-22 17:17:38 +0100

    adding tailwind and some components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header><h1 className='text-4xl uppercase font-bold'>Desde header</h1></header>
+    )
+}
+
+export default Header
+
+
+
